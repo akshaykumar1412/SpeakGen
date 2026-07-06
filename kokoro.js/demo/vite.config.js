64ddb776ca55e76d02d3414235a8e62384f294d8
Akshay <006akshay@gmail.com>
2026-07-06 22:02:59 +0530

    Add Vite alias for local Kokoro build

--- kokoro.js/demo/vite.config.js
+++ kokoro.js/demo/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "kokoro-js": fileURLToPath(new URL("../dist/kokoro.web.js", import.meta.url)),
+    },
+  },
   worker: { format: "es" },
   build: {
     target: "esnext",
