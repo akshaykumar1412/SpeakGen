b59b44d1d2d05060154dbcab0842e1bb326b5a74
Akshay <006akshay@gmail.com>
2026-07-06 22:09:47 +0530

    Use generated Kokoro vendor build in demo

--- kokoro.js/demo/vite.config.js
+++ kokoro.js/demo/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "kokoro-js": fileURLToPath(new URL("./src/vendor/kokoro.web.js", import.meta.url)),
+    },
+  },
+  optimizeDeps: {
+    exclude: ["kokoro-js"],
+  },
   worker: { format: "es" },
   build: {
     target: "esnext",
